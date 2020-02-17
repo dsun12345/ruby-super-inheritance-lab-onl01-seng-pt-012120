b72f7f536dd07e66bb63bc03a375ff1147b71357
Daniel Sun <danielsun12345@gmail.com>
2020-02-17 06:18:49 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,16 +7,8 @@ class ChattyStudent < Student
   
   def raise_hand 
     super 
-    puts "Pick me!"   
-    puts "Pick me!" 
-    puts "Pick me!" 
-    puts "Pick me!" 
-    puts "Pick me!" 
-    puts "Pick me!" 
-    puts "Pick me!" 
-    puts "Pick me!" 
-    puts "Pick me!" 
-    puts "Pick me!" 
+    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!"   
+    
   end 
   
 end 
